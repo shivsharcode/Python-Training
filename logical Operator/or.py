9ea446a36f32cc28410c3f63ef344f6f7de13d3f
shivsharcode <shivshar16@outlook.com>
2024-05-26 22:45:07 +0530

    day 26-05

---
+++ logical Operator/or.py
@@ -0,0 +1,12 @@
+"""
+if applicant has high income AND good credit => Eligible for loan
+"""
+
+has_high_income = True
+has_good_credit = False
+
+if has_high_income or has_good_credit:
+    print("Eligible for loan")
+else:
+    print("Ineligible for loan")
+

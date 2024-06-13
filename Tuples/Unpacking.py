fa7d883b3a9e5691ed751549ef4b806fe9cac289
shivsharcode <shivshar16@outlook.com>
2024-06-13 22:03:42 +0530

    Unpacking

---
+++ Tuples/Unpacking.py
@@ -0,0 +1,22 @@
+coordinates = (1, 2, 3)  # can be list, tuple, dictionary, nested structure,
+
+"""You have to multiply the three coordinates"""
+
+
+"way1"
+way1 = coordinates[0] * coordinates[1] * coordinates[2]
+
+"way2"
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+
+way2 = x * y * z
+
+"way- Unpacking"
+m, n, o = coordinates
+
+way3 = m * n * o
+print(way3)
+
+

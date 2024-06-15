4dd922cabf96b81194d0d5afaa244c13ea00c9af
shivsharcode <shivshar16@outlook.com>
2024-06-15 22:08:51 +0530

    dictionary day-2

--- Dictionaries/dictionary1.py
+++ Dictionaries/dictionary1.py
@@ -10,3 +10,14 @@ customer = {
 # note the keys should be unique
 
 print(customer["name"])
+# if the key is not present it will throw keyError :
+
+try:
+    print(customer["sex"])
+except KeyError as e:
+    print(f"KeyError : {e}")
+
+print(customer.get("age"))
+
+# get doesn't throw keyError , rather it gives None
+print(customer.get("sex"))

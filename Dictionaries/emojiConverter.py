cf833044c0ac612ec52f2cea1e292601f404d347
shivsharcode <shivshar16@outlook.com>
2024-07-15 23:41:22 +0530

    updated emoji convertor

--- Dictionaries/emojiConverter.py
+++ Dictionaries/emojiConverter.py
@@ -1,3 +1,20 @@
+"""
+EMOJI CONVERTOR
+"""
+
 message = input(">")
-words = message.split(' ') # .split splits the string into words and returns a list
-print(words)
+words = message.split(' ')  # split splits the string into words and returns a list
+
+emojis = {
+    ":)": "☺️",
+    ":(": "😔",
+    ":|": "😑"
+}
+
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+
+print(output)
+
+
